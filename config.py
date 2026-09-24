69f2b6b13c0897e7b99821a753178b4dace72d1e
swamysg19-blip <swamysg19@gmail.com>
2026-09-24 09:21:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+# BotFather inda banda token
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8560444041:AAEZ65J_BmP6qr4_fHXoMaJ5260AUY1Nh7Y")[cite: 2]
+
+# Telegram API ID & Hash (my.telegram.org inda barutte)
+API_ID = int(os.environ.get("API_ID", "YOUR_API_ID"))
+API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+
+# MongoDB Connection URL
+MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://swamysg19_db_user:8YRqU4lnIezTB8WC@cluster0.viihgnx.mongodb.net/?appName=Cluster0")[cite: 8, 10]
+
+# File save aguva channel ID
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100xxxxxxxxxx"))
